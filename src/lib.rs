fe59a36966d88dc85ca7e42eb9707d2b90806100
babariviere <babariviere@protonmail.com>
2017-01-08 17:03:33 +0100

    Add formatter

---
+++ src/lib.rs
@@ -0,0 +1,94 @@
+
+/// Formatter is a class to format a set of string with number.
+///
+/// # Example
+/// ```
+/// use Formatter;
+/// let mut formatter = Formatter::new("hello###", vec!["yo", "yey"]);
+/// let formatted = Formatter.iter().collect();
+/// assert_eq!(formatted, vec!["hello001", "hello002"]);
+/// ```
+pub struct Formatter {
+    format_types: Vec<FormatType>,
+    to_format: Vec<String>,
+    idx: usize,
+}
+
+impl Formatter {
+    pub fn new<S: AsRef<str>>(format: S, to_format: Vec<S>) -> Formatter {
+        let format_types = Formatter::build_format_types(&format);
+        Formatter {
+            format_types: format_types,
+            to_format: to_format.iter().map(|s| s.as_ref().to_string()).collect(),
+            idx: 0,
+        }
+    }
+
+    /// Build format types for Iterator.
+    fn build_format_types<S: AsRef<str>>(format: S) -> Vec<FormatType> {
+        let format = format.as_ref();
+        let mut ft = Vec::new();
+        let mut current: Option<FormatType> = None;
+        for c in format.chars() {
+            match c {
+                '#' => {
+                    match current {
+                        Some(FormatType::Index(z)) => current = Some(FormatType::Index(z + 1)),
+                        Some(current_c) => {
+                            ft.push(current_c);
+                            current = Some(FormatType::Index(1));
+                        }
+                        None => current = Some(FormatType::Index(1)),
+                    }
+                }
+                c => {
+                    match current {
+                        Some(FormatType::Text(s)) => {
+                            current = Some(FormatType::Text(format!("{}{}", s, c)))
+                        }
+                        Some(current_c) => {
+                            ft.push(current_c);
+                            current = Some(FormatType::Text(c.to_string()));
+                        }
+                        None => current = Some(FormatType::Text(c.to_string())),
+                    }
+                }
+            }
+        }
+        ft.push(current.unwrap_or(FormatType::Text("".to_string())));
+        ft
+    }
+
+    fn add_zeros(num: usize, num_zeros: usize) -> String {
+        let mut result = num.to_string();
+        while result.len() < num_zeros {
+            result.insert(0, '0');
+        }
+        result
+    }
+}
+
+impl Iterator for Formatter {
+    type Item = String;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.idx >= self.to_format.len() {
+            return None;
+        }
+        let mut result = String::new();
+        for ft in &self.format_types {
+            match ft {
+                &FormatType::Text(ref s) => result.push_str(s),
+                &FormatType::Index(z) => result.push_str(&Formatter::add_zeros(self.idx, z)),
+            }
+        }
+        self.idx += 1;
+        Some(result)
+    }
+}
+
+/// Provide format type to add to string result.
+#[derive(Clone, Debug)]
+enum FormatType {
+    Text(String),
+    Index(usize),
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+extern crate bulkro;
 extern crate clap;
 
+use bulkro::Formatter;
 use clap::{App, Arg};
 
 fn main() {
@@ -7,19 +9,24 @@ fn main() {
         .version("1.0")
         .author("notkild <notkild@gmail.com>")
         .about("Bulk renamer and organizer")
+        .arg(Arg::with_name("format")
+            .value_name("FORMAT")
+            .takes_value(true)
+            .required(true)
+            .help("Set files format"))
         .arg(Arg::with_name("files")
             .value_name("FILE")
             .takes_value(true)
             .multiple(true)
             .required(true)
             .help("Set the file to rename"))
-        .arg(Arg::with_name("format")
-            .value_name("FORMAT")
-            .takes_value(true)
-            .required(true)
-            .help("Set files format"))
         .get_matches();
 
-    let files = app.values_of("files").unwrap();
+    let files = app.values_of("files").unwrap().collect::<Vec<&str>>();
     let format = app.value_of("format").unwrap();
+    let formatter = Formatter::new(format, files);
+    let formatted = formatter.collect::<Vec<String>>();
+    for f in formatted {
+        println!("{}", f);
+    }
 }
