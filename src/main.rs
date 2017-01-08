49a6c6a3c7bf1c16b199421c8c518d2acd3b206b
babariviere <babariviere@protonmail.com>
2017-01-08 16:11:32 +0100

    Add argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+extern crate clap;
+
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let app = App::new("bulkro")
+        .version("1.0")
+        .author("notkild <notkild@gmail.com>")
+        .about("Bulk renamer and organizer")
+        .arg(Arg::with_name("files")
+            .value_name("FILE")
+            .takes_value(true)
+            .multiple(true)
+            .required(true)
+            .help("Set the file to rename"))
+        .arg(Arg::with_name("format")
+            .value_name("FORMAT")
+            .takes_value(true)
+            .required(true)
+            .help("Set files format"))
+        .get_matches();
+
+    let files = app.values_of("files").unwrap();
+    let format = app.value_of("format").unwrap();
 }
